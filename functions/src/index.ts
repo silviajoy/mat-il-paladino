f5f6a252adbcf56e2966a46c56f93e28f214608c
silviajoy <florio.sg@gmail.com>
2019-02-24 17:58:50 +0100

    action setup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,16 @@
 import * as functions from 'firebase-functions';
+import { dialogflow } from 'actions-on-google';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Instantiate the Dialogflow client.
+const app = dialogflow({debug: true});
+
+// Handle the Dialogflow intent named 'favorite color'.
+// The intent collects a parameter named 'color'.
+app.intent('favorite color', (conv, {color}) => {
+  const luckyNumber = color.length;
+  const output = `<speak>Il tuo numero fortunato è... ${luckyNumber}!</speak>`;
+    conv.close(output)
+});
+
+// Set the DialogflowApp object to handle the HTTPS POST request.
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
