f39c94ea5ccd383935bcce0c466f2147608e5c82
silviajoy <florio.sg@gmail.com>
2019-02-24 20:13:30 +0100

    deleted favorite color, added elementi comprati

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,25 @@
 import * as functions from 'firebase-functions'
 import { dialogflow } from 'actions-on-google'
+import moment from 'moment'
+import 'moment/locale/it'
+
+moment.locale('it')
 
 // Instantiate the Dialogflow client.
 const app = dialogflow({debug: true})
 
 // Handle the Dialogflow intent named 'favorite color'.
 // The intent collects a parameter named 'color'.
-app.intent('favorite color', (conv, {color}) => {
-    if(typeof color == 'string') {
-        const luckyNumber = color.length
-        const output = `<speak>Il tuo numero fortunato è... ${luckyNumber}!</speak>`
-        conv.close(output)
-    } 
+app.intent('elementi comprati', (conv, parameters) => {
+    let date = ''
+    let qty = `${parameters.number} ${parameters.udm}`
+    if(parameters.number == 1 && parameters.udm === 'porzioni') {
+        qty = `${parameters.number} porzione`
+    }
+    if(parameters.date) {
+        date = moment(parameters.date).format('LL')
+    }
+    conv.close(`Ok, ho aggiunto ${qty} di ${parameters.any} che scade il ${date}`)
 });
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
